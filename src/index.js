823ffbc88adf2a06ac4de3763b10f345acbabb66
Ed <mail@544062.ru>
2023-01-17 19:04:27 +0300

    feat: final solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const pairs = new Set
+  const same = new Set
+  const stack = []
+    
+  bracketsConfig.map( el => { pairs[el[0]] = el[1] })
+
+  for (const sym of str) {
+
+    if (pairs[sym] && !same[sym]) {
+      stack.push(sym);
+      if (pairs[sym] == sym) {
+        same[sym] = true
+      }
+    } 
+    else {  
+      last = stack.pop()
+      if (!last) {
+        return false;
+      }
+      if (same[sym]) {
+        same[sym] = false
+      }
+      if (pairs[last] != sym) {
+        return false
+      }  
+    }
+  }
+  return stack.length === 0;
 }
+
